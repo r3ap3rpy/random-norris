2e2b66c16629b7ad8ccc29188860555273a0bd29
Dániel Ernő Szabó <daniel.erno.szabo@LEGO.com>
2018-10-23 21:52:56 +0200

    Finalized the magnificient random norris repository.

---
+++ README.md
@@ -0,0 +1,8 @@
+### This repo holds a silly project
+The aim was to create a page which uses websockets and asyncio.
+The specialty of this project is that the box holds Chuck Norris jokes.
+The other specialty of the project is that every people watching the page gets to see the same joke, and anyone can request a new joke.
+
+> Never look a gift Chuck Norris in the mouth, because he will bite your damn eyes off.
+
+> Look at the magnificient [API](http://www.icndb.com/)
---
+++ norris.py
@@ -0,0 +1,51 @@
+import asyncio
+import json
+import logging
+import websockets
+import random
+import requests
+
+logging.basicConfig()
+
+STATE = {'value':json.loads(requests.get(url="http://api.icndb.com/jokes/random/").text)['value']['joke']}
+
+USERS = set()
+
+def state_event():
+	return json.dumps({'type':'state',**STATE})
+
+def users_event():
+	return json.dumps({'type':'users','count':	len(USERS)})
+
+async def notify_state():
+	if USERS:
+		print('Button clicked, sending state')
+		await asyncio.wait([user.send(state_event()) for user in USERS])
+
+async def notify_users():
+	if USERS:
+		print('User connected, sending state')
+		await asyncio.wait([user.send(users_event()) for user in USERS])
+		await asyncio.wait([user.send(state_event()) for user in USERS])
+
+async def register(websocket):
+	USERS.add(websocket)
+	await notify_users()
+
+async def unregister(websocket):
+	USERS.remove(websocket)
+	await notify_users()
+
+async def counter(websocket, path):
+	await register(websocket)
+	try:
+		await websocket.send(state_event())
+		async for message in websocket:
+			data = json.loads(message)
+			STATE['value'] = json.loads(requests.get(url="http://api.icndb.com/jokes/random/").text)['value']['joke']
+			await notify_state()
+	finally:
+		await unregister(websocket)
+
+asyncio.get_event_loop().run_until_complete(websockets.serve(counter, 'localhost',6789))
+asyncio.get_event_loop().run_forever()
